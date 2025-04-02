998572d6ce936bfc5d6c5eaa79bec996afae4c54
sarcasticDweller <capslocked.strife@gmail.com>
2025-04-01 20:58:55 -0400

    Loon has its own file and class

---
+++ loon.py
@@ -0,0 +1,40 @@
+# python libraries
+import random
+# project libraries
+import map
+
+class Loon:
+    def __init__(self, room):
+        self.moods = ["cranky", "mellow", "asleep"]
+        self.mood = self.set_mood()
+        """
+        cranky: lethal & mobile
+        mellow: passive & mobile
+        asleep: passive & immobile
+        """
+        self.room = room
+        self.room_previous = room
+        self.turns_not_moving = 0
+        self.turns_not_moving_max = 3
+    
+    def set_mood(self):
+        # note: original code set this to go whenever the turns left was even. should create logic to do this every other turn
+        self.mood = random.choice(self.moods)
+
+    def move_room(self):
+        self.room_previous = self.room
+        choices = map.get_directions_entity_can_move(self.room)
+        room_picked = random.choice(choices)
+        self.room = map.get_room_entity_moved_to(self.room, room_picked)
+        self.turns_not_moving = 0
+
+    def nudge_if_sleeping_too_long(self):
+        if self.turns_not_moving >= self.turns_not_moving_max:
+            self.move_room()
+    
+    def update(self):
+        self.set_mood()
+        if self.mood != "asleep":
+            self.move_room()
+        self.nudge_if_sleeping_too_long()
+        
--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 # python libraries
-import random, os, math
+import random, math
 # project libraries
 import ui as ttui
+from loon import Loon
 import map
 
 moods=["cranky", "mellow", "asleep"]
-"""
-cranky: lethal & mobile
-mellow: passive & mobile
-asleep: passive & immobile
-"""
 
 choices=[] # possibly useless?
 
 # variable declarations
 player_room = 0
-loon_room = 0
 bats_room = 0 
-loon_room_prev=loon_room
-turns_not_moving=0
-loon_mood=random.choice(moods)
 #Variable declarations
 turns_left=50 #Default is 50
-debug = 1
+debug = 0
 status=4
 """
 Status meanings:
@@ -47,14 +39,15 @@ def place_entities_in_rooms():
 
 def moveRoom():
     #Moves the player since it doesn't work well in the main loop
-    #global player_room #Prevents an error. :/
+    global player_room #Prevents an error. :/
     #Because borders freak Python out this is here.
 
     options = map.get_directions_entity_can_move(player_room)
     direction_chosen = ttui.list_menu("Which way would you like to go?", options)
     player_room = map.get_room_entity_moved_to(player_room, direction_chosen)
 
-def detectLoon(): # detects *distance* from player
+
+def detect_entity(reference, target):
     """
     Return value meanings:
     0: same room
@@ -62,22 +55,10 @@ def detectLoon(): # detects *distance* from player
     2: 2 rooms away
     ...
     """
-    proximity_code = 0
     detection_range = 2
-    relative_location, distance = map.get_neighboring_objects_at_range(player_room, loon_room, detection_range)
+    relative_location, distance = map.get_neighboring_objects_at_range(reference, target, detection_range)
     return distance
 
-def detectBats():
-    """
-    Return value meanings:
-    0: same room
-    1: 1 room away
-    2: 2 rooms away
-    ...
-    """
-    detection_range = 2
-    relative_location, distance = map.get_neighboring_objects_at_range(player_room, bats_room, detection_range)
-    return distance
 
 def get_random_flavor_text():
     statements=["You know he's here somewhere...",
@@ -87,36 +68,6 @@ def get_random_flavor_text():
     ]
     return random.choice(statements)
 
-def loonMood():
-    #Handles the randomization of the loon's mood.
-    #global loon_mood
-    if turns_left%2==0:
-        loon_mood=moods[random.randint(0, len(moods)-1)]
-
-def moveLoon(): 
-    #global loon_room       # oh
-    #global loon_room_prev  # my
-    #global turns_not_moving# why
-    loon_room_prev=loon_room
-    if loon_mood!=moods[2]:
-        #Generates choices
-        choices = map.get_directions_entity_can_move(loon_room)
-        #Loon picks a room
-        loon_room_picked = choices[random.randint(0, len(choices)-1)]
-        loon_room = map.get_room_entity_moved_to(loon_room, loon_room_picked)
-        
-    #Nudges the loon if he gets stuck for too long
-    if loon_room_prev==loon_room:
-        turns_not_moving+=1
-    else:
-        turns_not_moving=0
-    if turns_not_moving>=3:
-        try:
-            # why six? this is arbitrary.
-            loon_room=map.rooms[int(loon_room)+6] #6 to move up/down then left/right once each
-        except:
-            loon_room=map.rooms[int(loon_room)-6]
-        turns_not_moving=0
 
 def game_over(status):
     #Prints game-over messages
@@ -146,6 +97,7 @@ def print_debug_display():
     print("Status          : " + str(status))
     ttui.lines()
 
+loon = Loon(random.choice(map.rooms))
 #Main loop
 while True:
     # intro screen
@@ -155,14 +107,14 @@ while True:
     ttui.lines()
     answer = ttui.list_menu("Play?", ["Yes", "No"])
     if answer == "No":
-        quit
+        quit()
     if answer == "Yes":
         ttui.clear_screen()
     
     # game setup
-    player_room, loon_room, bats_room = place_entities_in_rooms()
+    player_room, loon.room, bats_room = place_entities_in_rooms()
     print(player_room)
-    print(loon_room)
+    print(loon.room)
     # game loop
     while True:
         if debug==1:
@@ -171,7 +123,8 @@ while True:
         ttui.show_stat_screen(player_room, turns_left)
 
         # tell user about how close the loon is
-        if detectLoon()==0: # same room, end game
+        loon_distance = detect_entity(player_room, loon.room)
+        if loon_distance == 0: # same room, end game
             if loon_mood==moods[0]:
                 ttui.clear_screen()
                 status=2
@@ -180,27 +133,27 @@ while True:
                 ttui.clear_screen()
                 status=3
                 break
-        elif detectLoon()==1:
+        elif loon_distance == 1:
             print("He's close.")
             print("He sounds as if he's " + loon_mood + ".")
-        elif detectLoon()==2:
+        elif loon_distance == 2:
             print("You hear the loon.")
         else:
             print(get_random_flavor_text())
         
-        # bats
-        if detectBats() == 0:
+        # bats & movement
+        bats_distance = detect_entity(player_room, bats_room)
+        if bats_distance == 0:
             print("You've awakened the bats.")
             player_room=map.get_random_room()
             bats_room=map.get_random_room()
             print("You hear rustling.")
-        elif detectBats() == 1:
+        elif bats_distance == 1:
             print("You hear rustling.")
         
         # logic to always run
         moveRoom() # runs regardless of awakening the bats, which makes things a bit unclear
-        loonMood()
-        moveLoon()
+        loon.update()
         ttui.clear_screen()
         if turns_left<1:
             status=0
